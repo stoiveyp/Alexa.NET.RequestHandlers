8fec98d4e2d0d8eca00b893b33f1a1dbc3da9895
Steven Pears <steven.pears@live.co.uk>
2018-05-19 19:47:50 +0100

    Add SynchronousRequestHandler and SynchronousErrorHandler

---
+++ Alexa.NET.RequestHandlers/Handlers/SynchronousErrorHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Alexa.NET.Response;
+
+namespace Alexa.NET.RequestHandlers.Handlers
+{
+    public abstract class SynchronousErrorHandler:IErrorHandler
+    {
+        public abstract bool CanHandle(RequestInformation request, Exception exception);
+
+        public abstract SkillResponse HandleSyncRequest(RequestInformation request, Exception exception);
+
+        public Task<SkillResponse> Handle(RequestInformation request, Exception exception)
+        {
+            return Task.FromResult(HandleSyncRequest(request, exception));
+        }
+    }
+}
---
+++ Alexa.NET.RequestHandlers/Handlers/SynchronousRequestHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Alexa.NET.Response;
+
+namespace Alexa.NET.RequestHandlers.Handlers
+{
+    public abstract class SynchronousRequestHandler:IRequestHandler
+    {
+        public abstract bool CanHandle(RequestInformation request);
+
+        public abstract SkillResponse HandleSyncRequest(RequestInformation request);
+
+        public Task<SkillResponse> Handle(RequestInformation request)
+        {
+            return Task.FromResult(HandleSyncRequest(request));
+        }
+    }
+}
