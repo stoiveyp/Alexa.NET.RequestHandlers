65ec38645a4ff4033ffcce4706d0d8de1a8d3814
Steven Pears <steven.pears@live.co.uk>
2018-08-22 07:21:04 +0100

    Add Launch request handlers (#4)
    * Add launch request handlers
    
    * Update NuGet package details

---
+++ Alexa.NET.RequestHandlers/Handlers/LaunchRequestHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+
+namespace Alexa.NET.RequestHandlers.Handlers
+{
+    public abstract class LaunchRequestHandler:IAlexaRequestHandler
+    {
+        public bool CanHandle(AlexaRequestInformation information)
+        {
+            return information.SkillRequest.Request is LaunchRequest;
+        }
+
+        public abstract Task<SkillResponse> Handle(AlexaRequestInformation information);
+    }
+}
